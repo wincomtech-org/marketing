f3cdbb72882f25f9fe5f7e759eb4518fe5068a80
luosader <greatsen@sina.com>
2017-09-14 17:11:20 +0800

    地址栏字

---
+++ languages/zh_cn/medium.lang.php
@@ -0,0 +1,10 @@
+<?php
+// 媒介中心
+$_LANG['medium_category'] = '媒介中心';
+$_LANG['medium_tree'] = '媒介分类';
+$_LANG['medium_content'] = '媒介介绍';
+$_LANG['medium_buy'] = '购买咨询';
+$_LANG['medium_news'] = '媒介展示';
+$_LANG['medium_more'] = '查看更多媒介';
+
+?>
